79dbbcc26b71b8cdaf7757216fbf047d7768b885
Luz M. Costa <luzmcosta@Lily.local>
2014-09-28 11:45:33 -0400

    Set copyright HTML.

--- templates/footer.php
+++ templates/footer.php
@@ -2,6 +2,7 @@
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+  <div class="copyright"><small>&copy; Copyright 2014, The Ugly Volvo<small></div>
 </footer>
 
 <?php wp_footer(); ?>
