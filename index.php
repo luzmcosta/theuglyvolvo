e0695671d90e71dc75bf7a8cc14f6c62a00bb995
Luz M. Costa <luzmcosta@Lily.local>
2014-09-28 02:22:20 -0400

    Sets language of no search results.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'roots'); ?>
+    <?php _e('No results were found.', 'roots'); ?>
   </div>
   <?php get_search_form(); ?>
 <?php endif; ?>
