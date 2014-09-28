ecd30d2b09a9ceba18f5dbbc1ac76e9c7be15d43
Luz M. Costa <luzmcosta@Lily.local>
2014-09-28 02:25:35 -0400

    Configures theme's widgets and menus.

--- lib/init.php
+++ lib/init.php
@@ -10,7 +10,9 @@ function roots_setup() {
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
   register_nav_menus(array(
-    'primary_navigation' => __('Primary Navigation', 'roots')
+    'primary_navigation' => __('Primary Navigation', 'roots'),
+    'social_navigation' => __('Social Navigation', 'roots'),
+    'share_navigation' => __('Share Navigation', 'roots')
   ));
 
   // Add post thumbnails
@@ -27,6 +29,18 @@ function roots_setup() {
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
   add_theme_support('html5', array('caption'));
 
+  $defaults = array(
+        'default-image'          => get_template_directory_uri() . '/assets/img/theuglyvolvo_header.jpg',
+        'random-default'         => false,
+        'width'                  => 750, // 838,
+        'height'                 => 500, // 559,
+        'flex-height'            => true,
+        'flex-width'             => true,
+        'default-text-color'     => '#000000',
+        'header-text'            => false
+  );
+  add_theme_support( 'custom-header', $defaults );
+
   // Tell the TinyMCE editor to use a custom stylesheet
   add_editor_style('/assets/css/editor-style.css');
 }
@@ -37,8 +51,17 @@ add_action('after_setup_theme', 'roots_setup');
  */
 function roots_widgets_init() {
   register_sidebar(array(
-    'name'          => __('Primary', 'roots'),
-    'id'            => 'sidebar-primary',
+    'name'          => __('Social', 'roots'),
+    'id'            => 'sidebar-social',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
+
+  register_sidebar(array(
+    'name'          => __('Share', 'roots'),
+    'id'            => 'sidebar-share',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
     'before_title'  => '<h3>',
--- templates/header.php
+++ templates/header.php
@@ -1,21 +1,33 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+  <div class="container-fluid">
+    <div class="social-navbar">
+      <div class="social">
+        <?php dynamic_sidebar('sidebar-social'); ?>
+      </div>
+
+      <div class="share">
+        <?php dynamic_sidebar('sidebar-share'); ?>
+      </div>
+
+      <div class="menu-icon">
+          <i class="fa fa-bars fa-3x"></i>
+      </div>
     </div>
 
-    <nav class="collapse navbar-collapse" role="navigation">
-      <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-        endif;
-      ?>
-    </nav>
+    <div class="header-brand">
+        <div>
+            <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo( get_header_image() ); ?>" alt="<?php echo( get_bloginfo( 'title' ) ); ?>" /></a>
+        </div>
+    </div>
+
+    <div class="header-navbar">
+        <nav class="collapse navbar-collapse" role="navigation">
+            <?php
+                if (has_nav_menu('primary_navigation')) :
+                  wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+                endif;
+            ?>
+        </nav>
+    </div>
   </div>
 </header>
