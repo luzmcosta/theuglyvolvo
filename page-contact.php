d642dd1e1387fcaed4db6f64d6447167a4b8bf75
Luz M. Costa <luzmcosta@Lily.local>
2014-10-19 20:50:25 -0400

    Adds template for contact page.

---
+++ page-contact.php
@@ -0,0 +1,17 @@
+<?php
+/* Template Name: Contact */
+?>
+
+<div id="primary" class="site-content">
+    <div id="content" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+    <div class="entry-content">
+        <?php the_content(); ?>
+    </div><!-- .entry-content -->
+
+    <?php endwhile; // end of the loop. ?>
+
+    </div><!-- #content -->
+</div><!-- #primary -->
