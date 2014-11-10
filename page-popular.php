cba26a34b9091e93b7fc3e3734ea5e581f18b62d
Luz M. Costa <luzmcosta@Lily.local>
2014-11-09 21:39:45 -0500

    Adds page template for 'Most Popular Posts' page.

---
+++ page-popular.php
@@ -0,0 +1,30 @@
+<?php
+/* Template Name: Posts by Most Popular */
+?>
+
+<div id="primary" class="site-content">
+    <div id="content" role="main">
+
+    <?php query_posts('orderby=comment_count'); ?>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+    <div class="entry-content most-popular-posts">
+        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+        <?php if ( get_the_post_thumbnail($post_id) != '' ) {
+                echo '<a href="'; the_permalink(); echo '" class="thumbnail-wrapper">';
+                the_post_thumbnail( array(275, 250) );
+                echo '</a>';
+            } else {
+                echo '<a href="'; the_permalink(); echo '" class="thumbnail-wrapper">';
+                echo '<img src="' . get_first_image() . '" alt="" />';
+                echo '</a>';
+            }
+        ?>
+    </div><!-- .entry-content -->
+
+    <?php endwhile; // end of the loop. ?>
+
+    </div><!-- #content -->
+</div><!-- #primary -->
