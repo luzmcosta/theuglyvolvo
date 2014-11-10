4db83ffeb81950d95910adb50a78fea12e3f6f65
Luz M. Costa <luzmcosta@Lily.local>
2014-11-09 21:38:29 -0500

    Structures archives' output.

--- archives.php
+++ archives.php
@@ -7,7 +7,16 @@
         <?php while ( have_posts() ) : the_post(); ?>
 
         <div class="entry-content">
-            <?php the_content(); ?>
+            <?php if ( get_the_post_thumbnail($post_id) != '' ) {
+                    echo '<a href="'; the_permalink(); echo '" class="thumbnail-wrapper">';
+                    the_post_thumbnail( array(275, 250) );
+                    echo '</a>';
+                } else {
+                    echo '<a href="'; the_permalink(); echo '" class="thumbnail-wrapper">';
+                    echo '<img src="' . get_first_image() . '" alt="" />';
+                    echo '</a>';
+                }
+            ?>
         </div><!-- .entry-content -->
 
         <?php endwhile; // end of the loop. ?>
