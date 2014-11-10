86c34d71443dd3306a4fe4879de6b35004f0e9d9
Luz M. Costa <luzmcosta@Lily.local>
2014-11-09 21:26:07 -0500

    Edits 404 page's content.

--- 404.php
+++ 404.php
@@ -1,13 +1,10 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
+  <?php _e('Sorry, but the page you are trying to view does not exist.', 'roots'); ?>
 </div>
 
-<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'roots'); ?></li>
-  <li><?php _e('an out-of-date link', 'roots'); ?></li>
-</ul>
+<p>This may be the result of either a mistyped address or an out-of-date link.
+    Perhaps a search will help you find your way.</p>
 
 <?php get_search_form(); ?>
