6c26b9d83b998d7a722b440b4eeb8a8ec3a44910
Luz M. Costa <luzmcosta@Lily.local>
2014-11-16 15:05:35 -0500

    Changes previous and next links' language.

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 <?php if ($wp_query->max_num_pages > 1) : ?>
   <nav class="post-nav">
     <ul class="pager">
-      <li class="previous"><?php next_posts_link(__('<span>Older</span>', 'roots')); ?></li>
-      <li class="next"><?php previous_posts_link(__('<span>Newer</span>', 'roots')); ?></li>
+      <li class="previous"><?php next_posts_link(__('<span>Older Posts</span>', 'roots')); ?></li>
+      <li class="next"><?php previous_posts_link(__('<span>Newer Posts</span>', 'roots')); ?></li>
     </ul>
   </nav>
 <?php endif; ?>
