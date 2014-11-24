0845c07ecaaca0939e29882eead345346ef7b7aa
Luz M. Costa <luzmcosta@Lily.local>
2014-11-24 00:02:18 -0500

    Sets custom tweet button at top of posts.

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,1 +1,3 @@
 <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+
+<a class="twitter-share" target="_blank" href="https://twitter.com/share?url=<?php the_permalink(); ?>&via=theuglyvolvo&related=luzmcosta&text=<?php the_title(); ?>"><i class="fa fa-twitter"></i></a>
