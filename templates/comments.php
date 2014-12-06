cd23efe7955ded71156e0a6c3e5165e4dce46d9b
Luz M. Costa <luzmcosta@Lily.local>
2014-12-06 15:06:02 -0500

    Sets most of comments' styles.

--- templates/comments.php
+++ templates/comments.php
@@ -6,7 +6,7 @@
 
 <section id="comments">
   <?php if (have_comments()) : ?>
-    <h3><?php printf(_n('One Response to &ldquo;%2$s&rdquo;', '%1$s Responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'roots'), number_format_i18n(get_comments_number()), get_the_title()); ?></h3>
+    <h3><?php printf(_n('One Comment', '%1$s Comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number()), get_the_title()); ?></h3>
 
     <ol class="media-list">
       <?php wp_list_comments(array('walker' => new Roots_Walker_Comment)); ?>
