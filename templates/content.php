5440dd2b74d5c4bbfbcc185193925f9aeffd6de3
Luz M. Costa <luzmcosta@Lily.local>
2014-12-06 22:34:35 -0500

    Refactors content.php.

--- templates/content.php
+++ templates/content.php
@@ -5,11 +5,13 @@
   </header>
   <div class="entry-summary">
     <?php if ( get_the_post_thumbnail($post_id) != '' ) {
-            echo '<a href="'; the_permalink(); echo '" class="thumbnail-wrapper">';
+            echo '<a href="'; the_permalink();
+            echo '" class="thumbnail-wrapper">';
             the_post_thumbnail( array(275, 250) );
             echo '</a>';
         } else {
-            echo '<a href="'; the_permalink(); echo '" class="thumbnail-wrapper">';
+            echo '<a href="'; the_permalink();
+            echo '" class="thumbnail-wrapper">';
             echo '<img src="' . get_first_image() . '" alt="" />';
             echo '</a>';
         }
