b4fdfc04f675fe50b346128c4fcc17938c046d40
Luz M. Costa <luzmcosta@Lily.local>
2014-12-06 23:33:51 -0500

    Removes a tag around the Facebook like button.

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,9 +1,7 @@
 <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 
 <div class="share-page">
-    <a class="fb-like" title="Like on Facebook" href="http://www.facebook.com/dialog/feed?app_id=700636043345419&display=popup&caption=<?php echo htmlentities(the_title()); ?>&link=<?php echo the_permalink(); ?>&redirect_uri=https://developers.facebook.com/tools/explorer" target="_blank">
-        <i class="fa fa-facebook"></i>
-    </a>
+    <i class="fa fa-facebook"></i>
 
     <a class="twitter-share" target="_blank" href="https://twitter.com/share?url=<?php the_permalink(); ?>&via=theuglyvolvo&related=luzmcosta&text=<?php the_title(); ?>">
         <i class="fa fa-twitter"></i>
