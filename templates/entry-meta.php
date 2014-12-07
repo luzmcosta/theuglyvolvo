b0860e6a700171c3d56cfb0d39756d24dd6f7d60
Luz M. Costa <luzmcosta@Lily.local>
2014-12-06 22:31:59 -0500

    Sets Facebook like button on pages.

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,3 +1,11 @@
 <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 
-<a class="twitter-share" target="_blank" href="https://twitter.com/share?url=<?php the_permalink(); ?>&via=theuglyvolvo&related=luzmcosta&text=<?php the_title(); ?>"><i class="fa fa-twitter"></i></a>
+<div class="share-page">
+    <a class="fb-like" title="Like on Facebook" href="http://www.facebook.com/dialog/feed?app_id=700636043345419&display=popup&caption=<?php echo htmlentities(the_title()); ?>&link=<?php echo the_permalink(); ?>&redirect_uri=https://developers.facebook.com/tools/explorer" target="_blank">
+        <i class="fa fa-facebook"></i>
+    </a>
+
+    <a class="twitter-share" target="_blank" href="https://twitter.com/share?url=<?php the_permalink(); ?>&via=theuglyvolvo&related=luzmcosta&text=<?php the_title(); ?>">
+        <i class="fa fa-twitter"></i>
+    </a>
+</div>
