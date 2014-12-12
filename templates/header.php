4cac0c9aa56d26b95e7c266979001a59ff84eb1b
Luz M. Costa <luzmcosta@Lily.local>
2014-12-11 22:23:35 -0500

    Sets on the header a sidebar for the search widget.

--- templates/header.php
+++ templates/header.php
@@ -1,17 +1,22 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container-fluid">
-    <div class="social-navbar">
-      <div class="social">
+
+      <div class="social-navbar">
+          <div class="search">
+              <?php dynamic_sidebar('sidebar-search'); ?>
+          </div>
+
+          <div class="social">
         <?php dynamic_sidebar('sidebar-social'); ?>
       </div>
 
       <div class="share">
         <?php dynamic_sidebar('sidebar-share'); ?>
       </div>
+    </div>
 
-      <div class="menu-icon">
-          <i class="fa fa-bars fa-3x"></i>
-      </div>
+    <div class="menu-icon">
+        <i class="fa fa-bars fa-3x"></i>
     </div>
 
     <div class="header-brand">
