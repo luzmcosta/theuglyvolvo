4b59973975fa96bf29d2cab6c2306e1176e3330d
Luz M. Costa <luzmcosta@Lily.local>
2014-12-14 00:12:22 -0500

    Sets header image's width to 90% of viewport given viewport width.

--- templates/header.php
+++ templates/header.php
@@ -26,25 +26,27 @@
             <a href="<?php echo esc_url(home_url('/')); ?>">
                 <picture>
                     <source media="(min-width: 960px)"
-                    srcset="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png 1x,
-                    <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png 1.5x,
-                    <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png 2x"
+                        srcset="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png 1x,
+                                <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png 1.5x,
+                                <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png 2x"
                     >
                     <source media="(min-width: 600px)"
-                    srcset="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1.5x.png 1x,
-                    <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1.5x.png 1.5x,
-                    <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1.5x.png 2x"
+                        sizes="90vw"
+                        srcset="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1.5x.png 1x,
+                                <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1.5x.png 1.5x,
+                                <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1.5x.png 2x"
                     >
                     <source media="(min-width: 300px)"
-                    srcset="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1x.png 1x,
-                    <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1x.png 1.5x,
-                    <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1x.png 2x"
+                        sizes="90vw"
+                        srcset="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1x.png 1x,
+                                <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1x.png 1.5x,
+                                <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1x.png 2x"
                     >
                     <img src="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png"
-                    alt="<?php echo( get_bloginfo( 'title' ) ); ?>"
-                    srcset="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1x.png 300w,
-                    <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1.5x.png 600w,
-                    <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png 960w"
+                        alt="<?php echo( get_bloginfo( 'title' ) ); ?>"
+                        srcset="<?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1x.png 300w,
+                                <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@1.5x.png 600w,
+                                <?php echo get_bloginfo('template_directory') ?>/assets/img/theuglyvolvo_logo@2x.png 960w"
                     />
                 <picture>
             </a>
