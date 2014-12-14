2487fc954eeda80ddf6bdf876b5b7eeebd091167
Luz M. Costa <luzmcosta@Lily.local>
2014-12-14 00:43:01 -0500

    Sets Pinterest post on above-post share buttons & updates related buttons accordingly.

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,9 +1,11 @@
 <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 
 <div class="share-page">
-    <i class="fa fa-thumbs-o-up like_page"></i>
+    <i class="fa fa-facebook like_page"></i>
+
+    <a href='javascript:void((function()%7Bvar%20e=document.createElement(&apos;script&apos;);e.setAttribute(&apos;type&apos;,&apos;text/javascript&apos;);e.setAttribute(&apos;charset&apos;,&apos;UTF-8&apos;);e.setAttribute(&apos;src&apos;,&apos;http://assets.pinterest.com/js/pinmarklet.js?r=&apos;+Math.random()*99999999);document.body.appendChild(e)%7D)());'><i class="fa fa-pinterest pin_page"></i></a>
 
     <a class="tweet_page" target="_blank" href="https://twitter.com/share?url=<?php the_permalink(); ?>&via=theuglyvolvo&related=luzmcosta&text=<?php the_title(); ?>">
-        <i class="fa fa-retweet"></i>
+        <i class="fa fa-twitter"></i>
     </a>
 </div>
