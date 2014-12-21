08b38c2a0f8ecdab5db9117a102edd6c085d9f90
Luz M. Costa <luzmcosta@Lily.local>
2014-12-21 15:08:51 -0500

    Adds meta description.

--- templates/head.php
+++ templates/head.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="description" content="A website for anyone with both a child and a sense of humor." />
   <meta property="og:url" content="<?php echo the_permalink(); ?>" />
   <meta property="og:title" content="<?php echo wp_title('|', true, 'right'); ?>" />
   <meta property="og:description" content="Attempts at Adulthood" />
